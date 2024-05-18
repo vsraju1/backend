eeb9cc708816ceb824e4b5407f71ba096fd778aa
V SHANMUKHA RAJU <rajunarayana1248@gmail.com>
2024-05-18 11:11:42 +0530

    updated user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -101,9 +101,14 @@ const loginUser = asyncHandler(async (req, res) => {
 
   const { email, username, password } = req.body;
 
-  if (!username || !email) {
+  if (!username && !email) {
     throw new ApiError(400, "username or email is required");
   }
+
+// alternate code for above code  
+//   if (!(username || email)) {
+//     throw new ApiError(400, "username or email is required");
+//   }
 
   const user = await User.findOne({
     $or: [{ username }, { email }],
